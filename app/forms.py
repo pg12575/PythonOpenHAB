24ba58924151650b3c32c66b217b6035fac9028b
pg12575 <prankit.gupta@brl.ac.uk>
2018-02-26 21:51:31 +0000

    Working upto task screen

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,8 @@ from wtforms.validators import DataRequired
 class IDForm(FlaskForm):
     ID = StringField('ID', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class Tasker(FlaskForm):
+    submit1 = SubmitField('Task A')
+    submit2 = SubmitField('Task B')
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,9 @@ import time
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import IDForm
+from app.forms import Tasker
+
 import simplejson as json
-
-
-@app.route('/loader')
-def loader():
-    return render_template('loader.html', title='Loading')
     
 @app.route('/',  methods=['GET', 'POST'])
 @app.route('/index',  methods=['GET', 'POST'])
@@ -17,7 +14,23 @@ def index():
         flash('Participant ID entered: {}'.format(form.ID.data))
         with open('file.json', 'w') as f:
             json.dump(form.ID.data, f)
-        return redirect(url_for('loader'))
+        return redirect(url_for('taskselect'))
     return render_template('index.html', title='Home', form=form)
 
 
+@app.route('/taskselect', methods=['GET', 'POST'])
+def taskselect():
+    form = Tasker()
+
+    if form.validate_on_submit():
+        if form.submit1.data:
+            return render_template('TaskA.html', title='Task A')
+        elif form.submit2.data:
+            return "Done B"
+    return render_template('taskselect.html', title='Tasks', form=form)
+
+
+
+@app.route('/TaskA')
+def TaskA():
+    return render_template('TaskA.html', title='Task A')
